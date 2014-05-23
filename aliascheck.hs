97ecf1d2a851b5e9648c48098803441192631ddb
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-05-23 10:52:26 -0500

    adding readFile, rawLine

--- aliascheck.hs
+++ aliascheck.hs
@@ -4,21 +4,30 @@ import Data.List (intercalate)
 
 type Parser t s = Parsec t s
 
-line, preAlias, validAlias, comment, groupAlias ::
+rawLine, validLine, preAlias, validAlias, comment, groupAlias ::
   	Parser [Char] st [Char]
 
-aliasFile = endBy line newline
+aliasFile = endBy rawLine newline
+rawLine = many (noneOf "\n")
 
-line = try comment <|> try groupAlias <|> validAlias
+validLine = try comment <|> try groupAlias <|> validAlias
+
+readLine :: [Char] -> [Char]
+readLine input = case parse validLine "" input of 
+	Left _ -> '#':input
+	Right _ -> input 
+
+readFile :: [Char] -> [[Char]]
+readFile input = map readLine (either (const $ error "Couldn't parse file") id (parse aliasFile "" input))
 
 validAlias = do
-	preAlias
+	_ <- preAlias
    	-- next line mostly works!
    	rest <- manyTill (noneOf "\n") (try $ ( angEmail <|> emailAddress ))
    	return rest
 
 groupAlias = do 
-	preAlias
+	_ <- preAlias
 	nicknames <- sepBy nickname (skipMany (char ' ') >> char ',' >> skipMany (char ' '))
 	-- this isn't actually what I want to return
 	return (concat nicknames)
@@ -27,12 +36,13 @@ comment = string "#" >> many (noneOf "\n")
 
 nickname = many (noneOf " ,\n")
 
-readLine :: [Char] -> [Char]
-readLine input = case parse line "" input of
-	Left _ -> '#':input
-	Right _ -> input
-
 preAlias = string "alias " >> manyTill anyChar space
+
+angEmail = do
+	char '<'
+	address <- emailAddress
+	char '>' <?> "closing angle bracket"
+	return address
 
 -- Using old emailAddress parser from Pandoc
 -- https://github.com/jgm/pandoc/blob/a71641a2a04c1d324163e16299f1e9821a26c9f9/src/Text/Pandoc/Parsing.hs
@@ -61,10 +71,4 @@ emailAddress = try $ do
     dom <- domain
     let full = addr ++ '@':dom
     return full
-
-angEmail = do
-	char '<'
-	address <- emailAddress
-	char '>' <?> "closing angle bracket"
-	return address
 
