d933f9c7538a2ad89e4bf13fc7e3a0427828951e
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-05-25 19:18:31 -0500

    fixing syntax, tabbing issues

--- aliascheck.hs
+++ aliascheck.hs
@@ -7,55 +7,55 @@ type Parser t s = Parsec t s
 main :: IO ()
 main = interact (unlines . sort . map checkLine . lines)
 
-preAlias, validAlias, comment, groupAlias, angEmail :: Parsec [Char] st [Char]
+preAlias, validAlias, comment, groupAlias, angEmail :: Parsec String st String
 
-checkLine :: [Char] -> [Char]
+checkLine :: String -> String
 checkLine input = case parse validLine "" input of 
-	Left _ -> '#':input
-	Right _ -> input 
-	where validLine = try comment <|> try groupAlias <|> validAlias
+    Left _ -> '#':input
+    Right _ -> input 
+    where validLine = try comment <|> try groupAlias <|> validAlias
 
 validAlias = do
-	_ <- preAlias >>
-         manyTill (noneOf "<>\n") (try ( angEmail <|> emailAddress ))
-	notFollowedBy anyToken <?> "end of input"
-   	return "valid single alias"
+    _ <- preAlias >>
+     manyTill (noneOf "<>\n") (try ( angEmail <|> emailAddress ))
+    notFollowedBy anyToken <?> "end of input"
+    return "valid single alias"
 
 groupAlias = do 
-	_ <- preAlias >> nickname >> nicknameSep 
-	_ <- sepBy nickname nicknameSep
-	return "valid group alias"
-	where
-      nickname = many (noneOf " ,\n")
-      nicknameSep = skipMany (char ' ') >> char ',' >> skipMany (char ' ')
+    _ <- preAlias >> nickname >> nicknameSep 
+    _ <- sepBy nickname nicknameSep
+    return "valid group alias"
+    where
+       nickname = many (noneOf " ,\n")
+       nicknameSep = skipMany (char ' ') >> char ',' >> skipMany (char ' ')
 
 comment = string "#" >> many (noneOf "\n")
 
 preAlias = string "alias " >> manyTill anyChar space
 
 angEmail = do
-	_ <- char '<' >> emailAddress >> char '>'
-	return "bracketed email address"
+    _ <- char '<' >> emailAddress >> char '>'
+    return "bracketed email address"
 
 -- Using modified old emailAddress parser from Pandoc
 -- https://github.com/jgm/pandoc/blob/a71641a2a04c1d324163e16299f1e9821a26c9f9/src/Text/Pandoc/Parsing.hs
 
-emailChar :: Parser [Char] st Char
+emailChar :: Parser String st Char
 emailChar = alphaNum <|> oneOf "!\"#$%&'*+-/0123456789=?^_{|}~"
 
-domain :: Parser [Char] st [Char]
+domain :: Parser String st String
 domain = do
-  x <- subdomain
-  xs <- many (try $ char '.' >> subdomain)
-  return $ intercalate "." (x:xs)
+    x <- subdomain
+    xs <- many (try $ char '.' >> subdomain)
+    return $ intercalate "." (x:xs)
 
-subdomain :: Parser [Char] st String
+subdomain :: Parser String st String
 subdomain = many1 (emailChar <|> char '@')
 
-emailWord :: Parser [Char] st String
+emailWord :: Parser String st String
 emailWord = many1 emailChar  -- ignores possibility of quoted strings
 
-emailAddress :: Parser [Char] st String
+emailAddress :: Parser String st String
 emailAddress = try $ do
     firstLetter <- alphaNum
     firstDot <- optionMaybe (char '.')
