7d57d06917939bee7f4ebc2d0f0f4f80e5bf98d9
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-03-06 17:19:27 -0600

    Add word wrapping functions to hurlview
    Hat tip http://moreindirection.blogspot.com/2010/08/blog-post.html

--- hurlview.hs
+++ hurlview.hs
@@ -1,3 +1,4 @@
+import Data.Char (isSpace)
 import Data.List.Split (splitOneOf)
 import Network.URI (isURI)
 import System.IO
@@ -10,9 +11,12 @@ type Lines = [(LineNumber, HasURL, Line)]
 sp :: Int -> String
 sp n = concat $ replicate n " "
 
--- Builds Lines out of raw string
+wrapLines :: String -> [String]
+wrapLines s = map (concatMap trim . wrapLine 88) (lines s)
+
+-- Builds Lines out of raw strings
 parseLines :: String -> Lines
-parseLines s = zip3 [1..] checkURL (lines s)
+parseLines s = zip3 [1..] checkURL (wrapLines s)
    where isURI' x = isURI x && (last x /= ':')
          checkURL = foldr (\x xs -> if any isURI' $ splitOneOf " \"<>()" x
                                        then True:xs
@@ -26,7 +30,24 @@ numberLines ((n,u,l):ts) = if u
                     else (sp 5 ++ l):numberLines ts
 
 getURL :: String -> Int -> [String]
-getURL s n = foldr (\x xs -> if isURI x then x:xs else xs) [] (words $ lines s !! n)
+getURL s n = foldr (\x xs -> if isURI x then x:xs else xs) [] (splitOneOf " \"<>()" $ lines s !! n)
+
+-- Word wrapping functions
+trim :: String -> String
+trim = trimAndReverse . trimAndReverse
+  where trimAndReverse = reverse . dropWhile isSpace
+ 
+reverseBreak :: (a -> Bool) -> [a] -> ([a], [a])
+reverseBreak f xs = (reverse before, reverse after)
+  where (after, before) = break f $ reverse xs
+ 
+wrapLine :: Int -> String -> [String]
+wrapLine maxLen line 
+  | length line <= maxLen  = [line]
+  | any isSpace beforeMax  = beforeSpace : wrapLine maxLen (afterSpace ++ afterMax)
+  | otherwise              = beforeMax : wrapLine maxLen afterMax
+    where (beforeMax, afterMax) = splitAt maxLen line
+          (beforeSpace, afterSpace) = reverseBreak isSpace beforeMax
 
 main :: IO ()
 main = do 
@@ -39,4 +60,4 @@ main = do
    choice <- hGetLine tty
    let n = read choice :: Int
    hClose tty
-   putStrLn $ "You chose " ++ unlines (getURL contents (n-1))
+   putStrLn $ "You chose " ++ unlines (getURL (unlines $ wrapLines contents) (n-1))
