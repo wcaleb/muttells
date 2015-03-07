b6460ab567e55defae3eb36af7f401ed5e135eb3
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-03-06 22:28:15 -0600

    Implement hacky open URL command in main

--- hurlview.hs
+++ hurlview.hs
@@ -2,6 +2,7 @@ import Data.Char (isSpace)
 import Data.List.Split (splitOneOf)
 import Network.URI (isURI)
 import System.IO
+import System.Process
 
 type Line = String
 type LineNumber = Int
@@ -15,7 +16,7 @@ isURI' :: String -> Bool
 isURI' x = isURI x && (last x /= ':')
 
 wrapLines :: String -> String
-wrapLines s = unlines $ map trim $ concatMap (wrapLine 78) (lines s)
+wrapLines s = unlines $ map trim $ concatMap wrapLine (lines s)
 
 -- Builds Lines out of raw strings
 parseLines :: String -> Lines
@@ -28,9 +29,9 @@ parseLines s = zip3 [1..] checkURL (lines s)
 numberLines :: Lines -> [String]
 numberLines [] = []
 numberLines ((n,u,l):ts) = if u
-                    then (marg n ++ show n ++ " -> " ++ l):numberLines ts
-                    else (sp 8 ++ l):numberLines ts
-            where marg n = if n > 9 then sp 2 else if n > 99 then sp 1 else sp 3
+                    then (sp (marg n) ++ show n ++ " -> " ++ l):numberLines ts
+                    else (sp (marg n) ++ sp (4 - marg n) ++ sp 4 ++ l):numberLines ts
+            where marg x | x > 9 = 2 | x > 99 = 1 | otherwise = 3
 
 -- Word wrapping functions
 -- Adapted from https://gist.github.com/moreindirection/524460
@@ -42,13 +43,14 @@ reverseBreak :: (a -> Bool) -> [a] -> ([a], [a])
 reverseBreak f xs = (reverse before, reverse after)
   where (after, before) = break f $ reverse xs
  
-wrapLine :: Int -> String -> [String]
-wrapLine maxLen line 
+wrapLine :: String -> [String]
+wrapLine line 
   | length line <= maxLen  = [line]
-  | any isSpace beforeMax  = beforeSpace : wrapLine maxLen (afterSpace ++ afterMax)
+  | any isSpace beforeMax  = beforeSpace : wrapLine (afterSpace ++ afterMax)
   | otherwise              = [line]
     where (beforeMax, afterMax) = splitAt maxLen line
           (beforeSpace, afterSpace) = reverseBreak isSpace beforeMax
+          maxLen = 78
 
 -- Retrieve URL(s) from a line
 getURL :: String -> Int -> [String]
@@ -66,3 +68,4 @@ main = do
    let n = read choice :: Int
    hClose tty
    putStrLn $ "You chose " ++ unlines (getURL wrappedLines (n-1))
+   callCommand ("open \"" ++ init (unlines (getURL wrappedLines (n-1))) ++ "\"")
